5c1b8c9eb9f0f385898c791f9befdc21a9b2215f
venkateshbizzhubdev <venkateshbizzhubdev@gmail.com>
2025-09-14 18:49:15 +0530

    Add main page layout and gradient components; include logo images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-
+import { Gradient, Gradient1, Gradient2 } from "@/components/practice";
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div className="mx-auto flex h-screen max-w-5xl flex-col space-y-3 p-3">
+      <Gradient />
+      <Gradient1 />
+      <Gradient2 />
+    </div>
+  );
 }
