c9800e3982d745457ceb4ae163918b2984db4fbf
venkateshbizzhubdev <venkateshbizzhubdev@gmail.com>
2026-03-02 23:14:31 +0530

    Refactor Home component and remove unused practice components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import { Gradient, Gradient1, Gradient2 } from "@/components/practice";
-
 export default function Home() {
   return (
-    <div className="mx-auto flex h-screen max-w-5xl flex-col space-y-3 p-3">
-      <Gradient />
-      <Gradient1 />
-      <Gradient2 />
+    <div className="mx-auto h-screen w-full max-w-7xl pt-20 pb-10">
+      <Header />
     </div>
   );
 }
+
+const Header = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-bold tracking-tighter text-neutral-700">
+        Bento grids are cool, you should try it sometimes.
+      </h1>
+      <p className="mt-4 max-w-xl text-base text-neutral-500">
+        Discover innovative solutions that transform the way you work and
+        create. Our cutting-edge tools are designed to empower your creativity
+        and boost your productivity like never before.
+      </p>
+    </>
+  );
+};
